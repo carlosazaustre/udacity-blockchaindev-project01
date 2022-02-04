0725e2bcbb604753b5ab50c875ccb3789b4734e7
Carlos Azaustre <cazaustre@gmail.com>
2022-02-04 23:35:50 +0100

    implemented getBData method

--- src/block.js
+++ src/block.js
@@ -64,11 +64,21 @@ class Block {
      *     or Reject with an error.
      */
     getBData() {
-        // Getting the encoded data saved in the Block
-        // Decoding the data to retrieve the JSON representation of the object
-        // Parse the data to an object to be retrieve.
+        let self = this;
+        return new Promise((resolve, reject) => {
+            // Getting the encoded data saved in the Block
+            let encodedData = self.body;
+            // Decoding the data to retrieve the JSON representation of the object
+            let decodedData = hex2ascii(encodedData);
+            // Parse the data to an object to be retrieve.
+            let dataObject = JSON.parse(decodedData);
+            // Resolve with the data if the object isn't the Genesis block
+            if (dataObject === 'Genesis Block') {
+                reject(new Error('This is the Genesis Block'));
+            }
+            resolve(dataObject);
 
-        // Resolve with the data if the object isn't the Genesis block
+        });
 
     }
 
