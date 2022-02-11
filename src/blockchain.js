cb55785ad8023a6b1cfed77385dd81d072d99191
Carlos Azaustre <cazaustre@gmail.com>
2022-02-11 11:36:31 +0100

    fix: using array.find instead of array.filter

--- src/blockchain.js
+++ src/blockchain.js
@@ -170,7 +170,7 @@ class Blockchain {
   getBlockByHeight(height) {
     let self = this;
     return new Promise((resolve, reject) => {
-      let block = self.chain.filter((p) => p.height === height)[0];
+      let block = self.chain.find((p) => p.height === height);
       if (block) {
         resolve(block);
       } else {
